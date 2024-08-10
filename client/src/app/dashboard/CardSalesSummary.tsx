52f7c899a356ae1a716b8f414dc747e2094ae583
Babina081 <babitamoo333@gmail.com>
2024-08-10 15:56:44 +0545

    add sales summary header

--- client/src/app/dashboard/CardSalesSummary.tsx
+++ client/src/app/dashboard/CardSalesSummary.tsx
@@ -41,92 +41,102 @@ const CardSalesSummary = () => {
       {isLoading ? (
         <div className="">Loading...</div>
       ) : (
-        <div>
-          <div className="flex justify-between items-center mb-6 px-7 mt-5">
-            <div className="text-lg font-medium">
-              <p className="text-xs text-gray-400 ">Value</p>
-              <span className="text-2xl font-extrabold">
-                Rs
-                {(totalValueSum / 1000000).toLocaleString("en", {
-                  maximumFractionDigits: 2,
-                })}
-                m
-              </span>
-              <span className="text-green-500 text-sm ml-2">
-                <TrendingUp className="inl inline w-4 h-4 mr-1"></TrendingUp>
-                {averageChangePercentage.toFixed(2)}%
-              </span>
-            </div>
-            <select
-              className="shadow-sm border border-gray-300 bg-white p-2 rounded-md"
-              value={timeframe}
-              onChange={(e) => {
-                setTimeframe(e.target.value);
-              }}
-            >
-              <option value="daily">Daily</option>
-              <option value="weekly">Weekly</option>
-              <option value="monthly">Monthly</option>
-            </select>
-          </div>
-          {/* charts */}
-          <ResponsiveContainer width="100%" height={350} className="px-7">
-            <BarChart
-              data={salesData}
-              margin={{ top: 0, right: 0, left: -25, bottom: 0 }}
-            >
-              <CartesianGrid
-                strokeDasharray=""
-                vertical={false}
-              ></CartesianGrid>
-              <XAxis
-                dataKey="date"
-                tickFormatter={(value) => {
-                  const date = new Date(value);
-                  return `${date.getMonth() + 1}/${date.getDate()}`;
-                }}
-              ></XAxis>
-              <YAxis
-                tickFormatter={(value) => {
-                  return `$${(value / 1000000).toFixed(0)}m`;
-                }}
-                tick={{ fontSize: 12, dx: -1 }}
-                tickLine={false}
-                axisLine={false}
-              />
-              <Tooltip
-                formatter={(value: number) => [
-                  `$${value.toLocaleString("en")}`,
-                ]}
-                labelFormatter={(label) => {
-                  const date = new Date(label);
-                  return date.toLocaleDateString("en-US", {
-                    year: "numeric",
-                    month: "long",
-                    day: "numeric",
-                  });
-                }}
-              />
-              <Bar
-                dataKey="totalValue"
-                fill="#3182ce"
-                barSize={10}
-                radius={[10, 10, 0, 0]}
-              />
-            </BarChart>
-          </ResponsiveContainer>
-          {/* footer */}
+        <>
+          {/* HEADER */}
           <div>
+            <h2 className="text-lg font-semibold mb-2 px-7 pt-5">
+              Sales Summary
+            </h2>
             <hr />
-            <div className="flex justify-between items-center text-sm px-7 mb-4 mt-6">
-              <p>{salesData.length || 0} days</p>
-              <p className="text-sm">
-                Highest Sales Date:
-                <span className="font-bold">{highestValueDate}</span>
-              </p>
+          </div>
+          {/* body */}
+          <div>
+            <div className="flex justify-between items-center mb-6 px-7 mt-5">
+              <div className="text-lg font-medium">
+                <p className="text-xs text-gray-400 ">Value</p>
+                <span className="text-2xl font-extrabold">
+                  Rs
+                  {(totalValueSum / 1000000).toLocaleString("en", {
+                    maximumFractionDigits: 2,
+                  })}
+                  m
+                </span>
+                <span className="text-green-500 text-sm ml-2">
+                  <TrendingUp className="inl inline w-4 h-4 mr-1"></TrendingUp>
+                  {averageChangePercentage.toFixed(2)}%
+                </span>
+              </div>
+              <select
+                className="shadow-sm border border-gray-300 bg-white p-2 rounded-md"
+                value={timeframe}
+                onChange={(e) => {
+                  setTimeframe(e.target.value);
+                }}
+              >
+                <option value="daily">Daily</option>
+                <option value="weekly">Weekly</option>
+                <option value="monthly">Monthly</option>
+              </select>
+            </div>
+            {/* charts */}
+            <ResponsiveContainer width="100%" height={350} className="px-7">
+              <BarChart
+                data={salesData}
+                margin={{ top: 0, right: 0, left: -25, bottom: 0 }}
+              >
+                <CartesianGrid
+                  strokeDasharray=""
+                  vertical={false}
+                ></CartesianGrid>
+                <XAxis
+                  dataKey="date"
+                  tickFormatter={(value) => {
+                    const date = new Date(value);
+                    return `${date.getMonth() + 1}/${date.getDate()}`;
+                  }}
+                ></XAxis>
+                <YAxis
+                  tickFormatter={(value) => {
+                    return `$${(value / 1000000).toFixed(0)}m`;
+                  }}
+                  tick={{ fontSize: 12, dx: -1 }}
+                  tickLine={false}
+                  axisLine={false}
+                />
+                <Tooltip
+                  formatter={(value: number) => [
+                    `$${value.toLocaleString("en")}`,
+                  ]}
+                  labelFormatter={(label) => {
+                    const date = new Date(label);
+                    return date.toLocaleDateString("en-US", {
+                      year: "numeric",
+                      month: "long",
+                      day: "numeric",
+                    });
+                  }}
+                />
+                <Bar
+                  dataKey="totalValue"
+                  fill="#3182ce"
+                  barSize={10}
+                  radius={[10, 10, 0, 0]}
+                />
+              </BarChart>
+            </ResponsiveContainer>
+            {/* footer */}
+            <div>
+              <hr />
+              <div className="flex justify-between items-center text-sm px-7 mb-4 mt-6">
+                <p>{salesData.length || 0} days</p>
+                <p className="text-sm">
+                  Highest Sales Date:
+                  <span className="font-bold">{highestValueDate}</span>
+                </p>
+              </div>
             </div>
           </div>
-        </div>
+        </>
       )}
     </div>
   );
