a767b1654fde02fc1210c158bb74ce2ea39721f0
Babina081 <babitamoo333@gmail.com>
2024-08-12 09:33:23 +0545

    Update: format function placement

--- client/src/app/products/CreateProductModal.tsx
+++ client/src/app/products/CreateProductModal.tsx
@@ -28,18 +28,6 @@ const CreateProductModal = ({
     rating: 0,
   });
 
-  if (!isOpen) return null;
-
-  const labelCssStyles = "block text-sm font-medium text-gray-700";
-  const inputCssStyles =
-    "block w-full mb-2 p-2 border-2 border-gray-300 rounded-md";
-
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    onCreate(formData);
-    onClose();
-  };
-
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     setFormData({
@@ -51,11 +39,24 @@ const CreateProductModal = ({
     });
   };
 
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    onCreate(formData);
+    onClose();
+  };
+
+  if (!isOpen) return null;
+
+  const labelCssStyles = "block text-sm font-medium text-gray-700";
+  const inputCssStyles =
+    "block w-full mb-2 p-2 border-gray-500 border-2 rounded-md";
+
   return (
     <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full z-20 ">
       <div className=" relative top-20 mx-auto border w-96 p-5 bg-white shadow-lg rounded-md">
         <Header name="Create New Product"></Header>
-        <form action="" onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit} className="mt-5">
+          {/* product name */}
           <label htmlFor="productName" className={labelCssStyles}>
             Product Name
           </label>
@@ -67,6 +68,56 @@ const CreateProductModal = ({
             value={formData.name}
             className={inputCssStyles}
           />
+          {/* product price */}
+          <label htmlFor="productPrice" className={labelCssStyles}>
+            Price
+          </label>
+          <input
+            type="number"
+            name="price"
+            placeholder="Price"
+            onChange={handleChange}
+            value={formData.price}
+            className={inputCssStyles}
+          />
+          {/* stock quantity */}
+          <label htmlFor="stockQuantity" className={labelCssStyles}>
+            Stock Quantity
+          </label>
+          <input
+            type="number"
+            name="stockQuantity"
+            placeholder="StockQuantity"
+            onChange={handleChange}
+            value={formData.stockQuantity}
+            className={inputCssStyles}
+          />
+          {/* rating */}
+          <label htmlFor="rating" className={labelCssStyles}>
+            Rating
+          </label>
+          <input
+            type="number"
+            name="rating"
+            placeholder="Rating"
+            onChange={handleChange}
+            value={formData.rating}
+            className={inputCssStyles}
+          />
+          {/* create action */}
+          <button
+            type="submit"
+            className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          >
+            Create
+          </button>
+          <button
+            type="button"
+            className="bg-gray-500 hover:bg-gray-700 ml-2 px-4 py-2 text-white rounded"
+            onClick={onClose}
+          >
+            Cancel
+          </button>
         </form>
       </div>
     </div>
