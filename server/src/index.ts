1f156ec2226df53dca23ec93659981e454f4b1ba
Babina081 <babitamoo333@gmail.com>
2024-08-12 09:38:38 +0545

    Add: user routes and controllers added

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,6 +8,7 @@ import morgan from "morgan";
 // route imports
 import dashboardRoutes from "./routes/dashboardRoutes";
 import productRoutes from "./routes/productRoutes";
+import userRoutes from "./routes/userRoutes";
 
 // configuration
 dotenv.config();
@@ -22,6 +23,7 @@ app.use(cors());
 // routes
 app.use("/dashboard", dashboardRoutes);
 app.use("/products", productRoutes);
+app.use("/users", userRoutes);
 
 // server
 const port = process.env.PORT || 3001;
